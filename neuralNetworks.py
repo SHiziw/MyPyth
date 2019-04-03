f64e54173d08dbf375dba0cb532dfe4e8b935f8b
SHiziw <45631851+SHiziw@users.noreply.github.com>
2019-04-04 00:15:05 +0800

    add query details

--- neuralNetworks.py
+++ neuralNetworks.py
@@ -1,4 +1,6 @@
 import numpy as np
+# scipy. special for the sigmoid function expit() 
+import scipy.special
 # neural network class definition
 class neuralNetwork :
 	
@@ -18,6 +20,10 @@ class neuralNetwork :
 		# w12 w22 etc
 		self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
 		self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+		# learning rate 
+		self. lr = learningrate
+		# activation function is the sigmoid function 
+		self.activation_function = lambda x: scipy.special.expit(x)
 
 		pass
 		
@@ -26,5 +32,18 @@ class neuralNetwork :
 		pass
 		
 	# query the neural network
-	def query():
-		pass
+	def query(self, inputs_list):
+		# convert inputs list to 2d array 
+		inputs = np.array(inputs_list, ndmin= 2).T
+		# calculate signals into hidden layer 
+		hidden_inputs = np.dot( self.wih, inputs) 
+		# calculate the signals emerging from hidden layer 
+		hidden_outputs = self.activation_function( hidden_inputs) 
+		# calculate signals into final output layer 
+		final_inputs = np. dot( self.who, hidden_outputs) 
+		# calculate the signals emerging from final output layer
+		final_outputs = self.activation_function( final_inputs)
+		return final_outputs
+
+
+	
