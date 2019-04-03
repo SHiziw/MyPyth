75126d1cff6a824700688736d9d680bf4cdcf02c
SHiziws10 <yql332211@126.com>
2019-04-03 18:56:29 +0800

    add init

--- neuralNetworks.py
+++ neuralNetworks.py
@@ -3,7 +3,13 @@ class neuralNetwork :
 	
 	# initialise the neural network
 	def _init_(self, inputnodes, hiddennodes, outputnodes, learningrate): 
-		# 
+		# set number of nodes in each input, hidden, output layer
+		self.inodes = inputnodes
+		self.hnodes = hiddennodes
+		self.onodes = outputnodes
+		
+		# learning rate
+		self.lr = learningrate
 		pass
 		
 	# train the neural network
